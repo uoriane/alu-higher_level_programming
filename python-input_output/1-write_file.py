b2472eded787ae1e742ce20b555d82ca7ba63435
uoriane <o.uwineza@lustudent.com>
2025-03-13 01:28:05 -0700

    python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-''' function that returns the number of lines of a text file
-'''
+"""
+function that write a string to text (UTF8) & return total of character written
+"""
 
 
-def number_of_lines(filename=""):
-    ''' function: number_of_lines
-    '''
-    if filename == "" or type(filename) != str:
-        return 0
-    counter = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            counter += 1
-    return counter
+def write_file(filename="", text=""):
+    """ module write_file
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
